c2c01a21b1999469036a40f42f8f93b739b16448
khalil <khalillakhdharatc@gmail.com>
2022-12-06 14:15:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes ,Route } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import {
+  BrowserRouter as Router,Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+render() {
+return (
+<Router>
+<ul>
+  <li><Link to="/">Accueil</Link></li>
+  <li><Link to="/propos">A propos</Link></li>
+  <li><Link to="/contact">Contactez nous</Link></li>
+</ul>
+<h1> Bienvenu</h1>
+<Routes>
+  <Route path="/propos" element={<About/>}></Route>
+  <Route path="/contact" element={<Contact/>}></Route>
+</Routes>
+
+
+</Router>
+
+
+)
+
+}
 }
 
 export default App;
